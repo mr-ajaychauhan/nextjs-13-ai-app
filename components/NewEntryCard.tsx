b6abca0b8c99a5fbd8cb63d87b0e5c43202933a4
Ajay Chauhan <48796206+mr-ajaychauhan@users.noreply.github.com>
2023-06-25 16:08:29 +0530

    added new entry router compo.

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+'use client'
+
+import { createNewEntry } from "@/utils/api"
+import { useRouter } from "next/navigation"
 
 const NewEntryCard = () => {
+    const router = useRouter()
+    const handleOnClick = async () => {
+        const data = await createNewEntry()
+        router.push(`journal/${data.id}`)
+    }
     return (
         <div className='cursor-pointer overflow-hidden rounded-lg bg-white shadow'>
-            <div className='px-4 py-5 sm:p-6'>
-                <span className='text-3xl text-gray-400'>+ New Entry</span>
+            <div className='px-4 py-5 sm:p-6' onClick={handleOnClick}>
+                <span className='text-3xl text-gray-400'>New Entry</span>
             </div>
         </div>
     )
