9f2359856ea3b150e79cb57f78a8bb524dd644ff
Ajay Chauhan <48796206+mr-ajaychauhan@users.noreply.github.com>
2023-06-25 23:00:13 +0530

    added ai editor page

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,6 +2,7 @@ import EntryCard from '@/components/EntryCard'
 import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import Link from 'next/link'
 const getEntries = async () => {
     const user = await getUserByClerkId()
     const entries = await prisma.journalEntry.findMany({
@@ -25,7 +26,11 @@ const JournalPage = async () => {
             <h2 className="text-3xl mb-8">Journal</h2>
             <div className="grid grid-cols-3 gap-4 ">
                 <NewEntryCard />
-                {entries.map(entry => <EntryCard key={entry.id} entry={entry} />)}
+                {entries.map(entry =>
+                    <Link key={entry.id} href={`/journal/${entry.id}`}>
+                        <EntryCard entry={entry} />
+                    </Link>
+                )}
             </div>
         </div>
     )
--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
 
-const EntryCard = ({entry}) => {
+const EntryCard = ({ entry }) => {
+  const date = new Date(entry.createdAt).toDateString()
   return (
-    <div>{entry.id}</div>
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5 sm:px-6">{date}</div>
+      <div className="px-4 py-5 sm:p-6">summary</div>
+      <div className="px-4 py-4 sm:px-6">mood</div>
+    </div>
   )
 }
 
